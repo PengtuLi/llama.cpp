7966d5fd3e2987ff92cb7d92d53a4db5dd207280
Patrick <lpt373595331@gmail.com>
2025-08-26 12:35:25 +0000

    fix llama160m bug, still facing bug in vocab part

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 # pip install -e sparkinfer-py
 # pip install -e gguf-py
 cmake -B build -DGGML_CUDA=ON -DGGML_CUDA_DEBUG=ON -DGGML_CUDA_FORCE_CUBLAS=ON -DLLAMA_CURL=OFF -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
-cmake --build build --config Release  --target llama-simple -j12
+cmake --build build --config Release  --target llama-simple llama-speculative -j12
